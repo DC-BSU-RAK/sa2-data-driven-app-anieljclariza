9c3c5dadd8c444038978eda2de5a85e7c60cc29b
Aniel-J Clariza <ajdcz@proton.me>
2025-12-08 19:27:22 +0400

    button worked, nice

--- Executable Project Code/main.py
+++ Executable Project Code/main.py
@@ -8,23 +8,35 @@ icon = tk.PhotoImage(file=r"Executable Project Code\harry-potter_flaticon.com.pn
 
 main.title("A Harry Potter fan's Harry Potter Database")
 main.iconphoto(True, icon)
-main.geometry("800x600")
+main.geometry("1024x768")
 main.resizable(0,0)
 
-response = requests.get("https://api.potterdb.com/v1/characters?filter[name_cont]=Potter")
-
-if response.status_code == 200:
-    data = response.json()
+response = requests.get("https://api.potterdb.com/v1/characters?filter[name_cont]=Harry James Potter")
+    
+def button():
+    if response.status_code == 200:
+        data = response.json()
     
     characters = data.get("data", [])
     
-    print(f"Total characters: {len(characters)}")
+    output = f"Total characters: {len(characters)}"
     
     for c in characters:
         attrs = c["attributes"]
-        print(f"Name: {attrs.get('name')}")
-        print(f"House: {attrs.get('house')}")
-        print(f"Species: {attrs.get('species')}")
-        print("===")
-else:
-    print("Error: ", response.status_code)
+        
+        output += (
+            f"Name: {attrs.get('name')}"
+            f"House: {attrs.get('house')}"
+            f"Species: {attrs.get('species')}"
+            f"==="
+        )
+    
+    hpLabel.config(text={output})
+
+pressMe = Button(main, text="Harry James Potter", command=lambda:button())
+pressMe.pack(expand=True, fill=BOTH)
+
+hpLabel = Label(main, text="")
+hpLabel.pack(side=BOTTOM ,expand=True, fill=BOTH)
+
+mainloop()
