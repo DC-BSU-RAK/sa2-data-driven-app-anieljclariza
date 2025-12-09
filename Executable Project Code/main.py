0fb1223d9f7fadb27df79597ef79be80a80512cb
Aniel-J Clariza <ajdcz@proton.me>
2025-12-09 20:13:36 +0400

    Did some rough codes

--- Executable Project Code/main.py
+++ Executable Project Code/main.py
@@ -1,42 +1,12 @@
 import tkinter as tk
 from tkinter import *
 import requests
+from PIL import Image, ImageTk
+
+window_icon = tk.PhotoImage(file=r"Executable Project Code\harry-potter_flaticon.com.png")
+
+
 
 main = tk.Tk()
+main.iconphoto(window_icon)
 
-icon = tk.PhotoImage(file=r"Executable Project Code\harry-potter_flaticon.com.png")
-
-main.title("A Harry Potter fan's Harry Potter Database")
-main.iconphoto(True, icon)
-main.geometry("1152x864")
-main.resizable(0,0)
-
-response = requests.get("https://api.potterdb.com/v1/characters?filter[house]=gryffindor")
-    
-def button():
-    if response.status_code == 200:
-        data = response.json()
-    
-    characters = data.get("data", [])
-    
-    output = f"Total characters: {len(characters)}"
-    
-    for c in characters:
-        attrs = c["attributes"]
-        
-        output += (
-            f"Name: {attrs.get('name')}\n"
-            f"House: {attrs.get('house')}\n"
-            f"Species: {attrs.get('species')}\n"
-            f"===\n"
-        )
-    
-    hpLabel.config(text={output})
-
-pressMe = Button(main, text="Click me", command=lambda:button())
-pressMe.pack(fill=BOTH)
-
-hpLabel = Label(main, text="")
-hpLabel.pack(side=BOTTOM, fill=BOTH, expand=True)
-
-mainloop()
--- Executable Project Code/test_code.py
+++ Executable Project Code/test_code.py
@@ -1,21 +1,32 @@
-import requests
+import tkinter as tk # for gui
+from tkinter import * # import everything from tkinter
+import requests # for api
+from PIL import Image, ImageTk # for setting background image
 
-url = "https://api.potterdb.com/v1/characters"
+main = tk.Tk()
 
-response = requests.get(url)
+window_icon = tk.PhotoImage(file=r"Executable Project Code\harry-potter_flaticon.com.png")
 
-if response.status_code == 200:
-    data = response.json()
-    
-    # PotterDB stores items in data["data"]
-    characters = data.get("data", [])
-    
-    print(f"Total characters: {len(characters)}\n")
-    
-    for c in characters[:5]:
-        attrs = c["attributes"]
-        print(f"Name: {attrs.get('name')}")
-        print(f"Species: {attrs.get('species')}")
-        print("---")
-else:
-    print("Error:", response.status_code)
+background_image = Image.open(r"Executable Project Code\valerii-siserg-map3.jpg")
+background_image = background_image.resize((900, 700))
+background_photo = ImageTk.PhotoImage(background_image)
+
+main.title("A Harry Potter fan's Harry Potter Database Program")
+main.iconphoto(True, icon)
+main.geometry("900x700")
+main.config()
+main.resizable(0,0)
+
+onlyFrame = Frame(main)
+onlyFrame.place(relwidth=1, relheight=1)
+
+backgroundLabel = Label(onlyFrame)
+backgroundLabel.place(x=0, y=0, relwidth=1, relheight=1)
+backgroundLabel.config(image=background_photo)
+
+def switch_frame(frame):
+    frame.tkraise()
+
+switch_frame(onlyFrame)
+
+mainloop()
